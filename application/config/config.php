81506311bfeee6196a4b4b8fde241a3571f1dd2b
V1n1c1us <v.f.diehl@gmail.com>
2017-09-04 22:26:51 -0300

    Was Created - Save Session to Database

--- application/config/config.php
+++ application/config/config.php
@@ -377,11 +377,11 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'database'; // 
 $config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
-$config['sess_match_ip'] = FALSE;
+$config['sess_expiration'] = 0; // sessão será extinta quando o usuário fechar o browser (expira)
+$config['sess_save_path'] = 'ci_session'; //aonde que irá salvar a sessão (na tabela ci_session)
+$config['sess_match_ip'] = TRUE; // gravar o número do ip
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
 
